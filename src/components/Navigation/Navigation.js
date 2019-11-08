2f1d7c9ce1804e908071be6e1189306dee3980fe
Irene Rojas <irojas@plan-sys.com>
2019-11-08 14:36:22 -0500

    logo and slogan style

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import "./Navigation.css";
+import niccsLogo from "../../images/logo-dark.png";
  
 import { NavLink } from 'react-router-dom';
  
 const Navigation = () => {
     return (
        <div className="navDiv">
-          <NavLink to="/" className="navLinkHome">Home</NavLink>
+          <NavLink to="/" className="navLinkHome navText">
+             <img src={niccsLogo} id="niccsLogo" alt="National Initiative for Cybersecurity Careers and Studies" />
+             <h2 id="niccsSlogan">National Initiative for Cybersecurity Careers and Studies</h2>
+            </NavLink>
           <br/>
-          <NavLink to="/about" className="navLinkAbout">About</NavLink>
+          <NavLink to="/about" className="navLinkAbout navText">About</NavLink>
           <br/>
-          <NavLink to="/training" className="navLinkContact">Training</NavLink>
+          <NavLink to="/training" className="navLinkContact navText">Training</NavLink>
           <br/>
-          <NavLink to="/education" className="navLinkContact">Education</NavLink>
+          <NavLink to="/education" className="navLinkEdu navText">Education</NavLink>
           <br/>
-          <NavLink to="/develop" className="navLinkContact">Workforce Development</NavLink>
+          <NavLink to="/develop" className="navLinkDev navText">Workforce Development</NavLink>
        </div>
     );
 }
