978371712609e18eb3daed52aa72fee9abf796e0
Irene Rojas <irojas@plan-sys.com>
2019-11-08 14:51:33 -0500

    basic nav layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./Navigation.css";
 import niccsLogo from "../../images/logo-dark.png";
+import search from "../../images/search-dark.png";
  
 import { NavLink } from 'react-router-dom';
  
@@ -12,13 +13,12 @@ const Navigation = () => {
              <h2 id="niccsSlogan">National Initiative for Cybersecurity Careers and Studies</h2>
             </NavLink>
           <br/>
-          <NavLink to="/about" className="navLinkAbout navText">About</NavLink>
-          <br/>
-          <NavLink to="/training" className="navLinkContact navText">Training</NavLink>
-          <br/>
-          <NavLink to="/education" className="navLinkEdu navText">Education</NavLink>
-          <br/>
+          <NavLink to="/training" className="navLinkTraining navText">Training</NavLink>
+          <NavLink to="/education" className="navLinkEdu navText">Formal Education</NavLink>
           <NavLink to="/develop" className="navLinkDev navText">Workforce Development</NavLink>
+          <NavLink to="/about" className="navLinkAbout navText">About NICCS</NavLink>
+         <img src={search} alt="Search Button" id="search" />
+          
        </div>
     );
 }
