a5216d4282b3c527d74875617383b5252af4aee8
Irene Rojas <irojas@plan-sys.com>
2019-11-14 15:19:13 -0500

    added NoFear and Access from Drupal

--- src/components/Accessibility/Accessibility.js
+++ src/components/Accessibility/Accessibility.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Accessibility = () => {
+function Accessibility() {
+
+    const [access, setAccess] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://dev-irene-react.pantheonsite.io/jsonapi/node/page/3d023c31-4c00-4669-bb8f-52b05286b1d6`)
+        .then(res => {
+            setAccess(res.data.data.attributes.body);
+            // console.log(res.data.data.attributes.body);
+        })
+    }, []);
+
+
     return (
-        <div className="accessDiv">
-            <h1>Accessibility</h1>
-            <br/>
-            <p>NICCS and the Department of Homel​and Security are committed to providing accessible Information and Communication Technology (ICT) to individuals with disabilities, including members of the public and federal employees, by meeting or exceeding the requirements of Section 508 of the Rehabilitation Act of 1973. In addition, the Department is also committed to ensuring accessibility of our buildings and facilities as required by the Architectural Barriers Act, 42 U.S.C. 4151 through 4157.</p>
-            <br/>
-            <p>Section 508 of the Rehabilitation Act of 1973, as amended (29 U.S.C. 794d)</p>
-            <br/>
-            <p>Section 508 of the Rehabilitation Act of 1973, as amended requires agencies, during the procurement, development, maintenance, or use of ICT, to ensure that individuals with disabilities have access to and use of ICT information and data comparable to the access and use afforded to individuals without disabilities (i.e., “ICT accessibility"), unless an undue burden would be imposed on the agency. More information on Section 508 and the technical standards can be found at www.section508.gov.</p>
+        <div className="accessContentDiv">
+            <div className="accessBodyDiv">
+                {access.value}
+            </div>
+
         </div>
     )
 }
 
 
-
-
-
+// const Accessibility = () => {
+//     return (
+//         <div className="accessDiv">
+//             <h1>Accessibility</h1>
+//             <br/>
+//             <p>NICCS and the Department of Homel​and Security are committed to providing accessible Information and Communication Technology (ICT) to individuals with disabilities, including members of the public and federal employees, by meeting or exceeding the requirements of Section 508 of the Rehabilitation Act of 1973. In addition, the Department is also committed to ensuring accessibility of our buildings and facilities as required by the Architectural Barriers Act, 42 U.S.C. 4151 through 4157.</p>
+//             <br/>
+//             <p>Section 508 of the Rehabilitation Act of 1973, as amended (29 U.S.C. 794d)</p>
+//             <br/>
+//             <p>Section 508 of the Rehabilitation Act of 1973, as amended requires agencies, during the procurement, development, maintenance, or use of ICT, to ensure that individuals with disabilities have access to and use of ICT information and data comparable to the access and use afforded to individuals without disabilities (i.e., “ICT accessibility"), unless an undue burden would be imposed on the agency. More information on Section 508 and the technical standards can be found at www.section508.gov.</p>
+//         </div>
+//     )
+// }
 
 export default Accessibility;
--- src/components/NoFear/NoFear.js
+++ src/components/NoFear/NoFear.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const NoFear = () => {
+function NoFear() {
+
+    const [noFear, setNoFear] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://dev-irene-react.pantheonsite.io/jsonapi/node/page/63730352-7b79-465b-a66d-8ab542b802c9`)
+        .then(res => {
+            setNoFear(res.data.data.attributes.body);
+            // console.log(res.data.data.attributes.body);
+        });
+    }, []);
+
     return (
-        <div>
-            <h1>No Fear Act Policy</h1>
-            <br/>
-            <p>Section 202(a) of the Notification and Federal Employee Antidiscrimination and Retaliation (No FEAR) Act, Pub. L. 107-74, Title I, General Provisions, Section 101(1), requires each federal agency to provide written notification of the rights and protections available to federal employees, former federal employees, and applicants for federal employment under federal antidiscrimination and whistleblower laws listed in the No FEAR Act. In support of this requirement, Secretary Napolitano issued a memo to all Department employees (PDF - 2 pages, 805 KB) reiterating the Department’s commitment to the No FEAR Act of 2002. In addition, the Secretary issued a new anti-discrimination policy statement.</p>
-            <br/>
-            <p>5 CFR 724.202 requires federal agencies to publish this notification in the Federal Register. This Federal Register publication specifically provides notice to the general public that individuals seeking to pursue an Equal Employment Opportunity (EEO) complaint must contact a Department of Homeland Security EEO counselor within 45 calendar days of the alleged discriminatory action, or, in the case of a personnel action, within 45 calendar days of the effective date of the action.</p>
+        <div className="noFearContentDiv">
+            <div className="noFearBodyDiv">
+                {noFear.value}
+            </div>
         </div>
-    );
+    )
+
 }
+
+// const NoFear = () => {
+//     return (
+//         <div>
+//             <h1>No Fear Act Policy</h1>
+//             <br/>
+//             <p>Section 202(a) of the Notification and Federal Employee Antidiscrimination and Retaliation (No FEAR) Act, Pub. L. 107-74, Title I, General Provisions, Section 101(1), requires each federal agency to provide written notification of the rights and protections available to federal employees, former federal employees, and applicants for federal employment under federal antidiscrimination and whistleblower laws listed in the No FEAR Act. In support of this requirement, Secretary Napolitano issued a memo to all Department employees (PDF - 2 pages, 805 KB) reiterating the Department’s commitment to the No FEAR Act of 2002. In addition, the Secretary issued a new anti-discrimination policy statement.</p>
+//             <br/>
+//             <p>5 CFR 724.202 requires federal agencies to publish this notification in the Federal Register. This Federal Register publication specifically provides notice to the general public that individuals seeking to pursue an Equal Employment Opportunity (EEO) complaint must contact a Department of Homeland Security EEO counselor within 45 calendar days of the alleged discriminatory action, or, in the case of a personnel action, within 45 calendar days of the effective date of the action.</p>
+//         </div>
+//     );
+// }
 
 export default NoFear;
