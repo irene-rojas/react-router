5a9fd8d489b834b27d53de90b576b1e02e528942
Irene Rojas <irojas@plan-sys.com>
2019-11-15 12:30:15 -0500

    removed original text

--- src/components/NICCSPolicy/NICCSPolicy.js
+++ src/components/NICCSPolicy/NICCSPolicy.js
@@ -29,23 +29,4 @@ function NICCSPolicy() {
 
 }
 
-// const NICCSPolicy = (props) => {
-//     return (
-
-//         <div>
-//             <h1>NICCS Policy</h1>
-//             <br/>
-//             <h2>What is the NICCS Education and Training Catalog?</h2>
-//             <br/>
-//             <p>The NICCS Education and Training Catalog is a repository of cybersecurity courses offered throughout the United States.</p>
-//             <ul>
-//                 <li>The Training Catalog is a resource containing course information on more than 3,000 cybersecurity-related courses. The Catalog can be searched using a variety of criteria including: keyword, proficiency level, delivery method, and Workforce Framework Specialty Area.</li>
-//                 <li>NICCS provides summary course information and links to the training provider offering that course. All course materials are maintained by the organization or academic institution providing the training. This means when you access the link you will leave the Department of Homeland Security site. </li>
-//                 <li>The Training Catalog is updated as new course materials are received, vetted and added to the Catalog. The Training Catalog can be accessed at https://niccs.us-cert.gov/training/search.</li>
-//             </ul>
-//             <p>For information on the Privacy Policy, please visit the DHS Privacy Policy.</p>
-//         </div>
-//     );
-// }
-
 export default NICCSPolicy;
