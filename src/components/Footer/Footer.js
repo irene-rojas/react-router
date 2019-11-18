c74e82d0c6640810ec5f86cc30eb12246b7725b4
Irene Rojas <irojas@plan-sys.com>
2019-11-18 17:06:53 -0500

    footer words still wrapped but img in separate div

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,45 +8,51 @@ const Footer = () => {
     return (
        <div className="footerDiv">
 
-            <ul id="footerMenu" className="footerMenu">
-                <li id="footerHome" className="footerLine">
-                    <Link to="/" className="footerHome footerText">
-                        Home
-                    </Link>
-                </li>
+           <div className="footerMenuDiv">
 
-                <li id="footerPrivacy" className="footerLine">
-                    <span className="footerPrivacy footerText">Privacy Policy</span>
-                </li>
+                <ul className="footerList">
+                    <li id="footerHome" className="footerLine">
+                        <Link to="/" className="footerHome footerText">
+                            Home
+                        </Link>
+                    </li>
 
-                <li id="footerNICCS" className="footerLine">
-                    <Link to="/niccs_policy" className="footerNICCS footerText">
-                        NICCS Policy
-                    </Link>
-                </li>
+                    <li id="footerPrivacy" className="footerLine">
+                        <span className="footerPrivacy footerText">Privacy Policy</span>
+                    </li>
 
-                <li id="footerFOIA" className="footerLine">
-                    <Link to="foia_policy" className="footerFOIA footerText">
-                        FOIA Policy
-                    </Link>
-                </li>
+                    <li id="footerNICCS" className="footerLine">
+                        <Link to="/niccs_policy" className="footerNICCS footerText">
+                            NICCS Policy
+                        </Link>
+                    </li>
 
-                <li id="footerNoFear" className="footerLine">
-                    <Link to="/nofear" className="footerNoFear footerText">
-                        No Fear Act Policy
-                    </Link>
-                </li>
+                    <li id="footerFOIA" className="footerLine">
+                        <Link to="foia_policy" className="footerFOIA footerText">
+                            FOIA Policy
+                        </Link>
+                    </li>
 
-                <li id="footerAccess" className="footerLine">
-                    <Link to="/access" className="footerAccess footerText">
-                        Accessibility
-                    </Link>
-                </li>
-            </ul>
+                    <li id="footerNoFear" className="footerLine">
+                        <Link to="/nofear" className="footerNoFear footerText">
+                            No Fear Act Policy
+                        </Link>
+                    </li>
 
-            <div className="dhsLogoDiv">
-                <img src={dhsLogo} alt="DHS Logo" id="dhsLogo" />
+                    <li id="footerAccess" className="footerLine">
+                        <Link to="/access" className="footerAccess footerText">
+                            Accessibility
+                        </Link>
+                    </li>
+                </ul>
+
+                <div className="dhsLogoDiv">
+                    <img src={dhsLogo} alt="DHS Logo" id="dhsLogo" />
+                </div>
+
             </div>
+            {/* end footerMenu */}
+
 
        </div>
     );
