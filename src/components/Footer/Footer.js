cfb10de0a6434ad72a2904fa24d5345ab981221d
Irene Rojas <irojas@plan-sys.com>
2019-11-18 13:18:04 -0500

    added visual clarity in Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,12 +9,29 @@ const Footer = () => {
        <div className="footerDiv">
 
             <ul id="footerMenu" className="footerMenu">
-                <li id="footerHome" className="footerLine"><Link to="/" className="footerHome footerText">Home</Link></li>
-                <li id="footerPrivacy" className="footerLine"><span className="footerPrivacy footerText">Privacy Policy</span></li>
-                <li id="footerNICCS" className="footerLine"><Link to="/niccs_policy" className="footerNICCS footerText">NICCS Policy</Link></li>
-                <li id="footerFOIA" className="footerLine"><Link to="foia_policy" className="footerFOIA footerText">FOIA Policy</Link></li>
-                <li id="footerNoFear" className="footerLine"><Link to="/nofear" className="footerNoFear footerText">No Fear Act Policy</Link></li>
-                <li id="footerAccess" className="footerLine"><Link to="/access" className="footerAccess footerText">Accessibility</Link></li>
+                <li id="footerHome" className="footerLine">
+                    <Link to="/" className="footerHome footerText">Home</Link>
+                </li>
+
+                <li id="footerPrivacy" className="footerLine">
+                    <span className="footerPrivacy footerText">Privacy Policy</span>
+                </li>
+
+                <li id="footerNICCS" className="footerLine">
+                    <Link to="/niccs_policy" className="footerNICCS footerText">NICCS Policy</Link>
+                </li>
+
+                <li id="footerFOIA" className="footerLine">
+                    <Link to="foia_policy" className="footerFOIA footerText">FOIA Policy</Link>
+                </li>
+
+                <li id="footerNoFear" className="footerLine">
+                    <Link to="/nofear" className="footerNoFear footerText">No Fear Act Policy</Link>
+                </li>
+
+                <li id="footerAccess" className="footerLine">
+                    <Link to="/access" className="footerAccess footerText">Accessibility</Link>
+                </li>
             </ul>
 
             <div className="dhsLogoDiv">
