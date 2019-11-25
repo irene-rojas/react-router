f72c87cd54eaa74b4f75f2e5608a42c34aa0189c
Irene Rojas <irojas@plan-sys.com>
2019-11-25 15:12:39 -0500

    training backup

--- src/components/Training/Training.js
+++ src/components/Training/Training.js
@@ -11,7 +11,7 @@ function Training() {
       axios.get(`https://dev-irene-react.pantheonsite.io/jsonapi/node/page/cd81c043-92ec-4f2e-926b-96a0bfcfeae7`)
       .then(res => {
         setTrainingTitle(res.data.data.attributes.title);
-        // console.log(res.data.data.attributes);
+        console.log(res.data.data.attributes);
         setTrainingBody(res.data.data.attributes.body);
       });
     }, []);
